935c979523c8a5bca2c42badb7516b1e76175c21
jenith-acharya <kan077bct041@kec.edu.np>
2025-01-08 21:58:42 +0545

    model file changes

--- LLS back/src/modules/blogs/blogs.model.js
+++ LLS back/src/modules/blogs/blogs.model.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const BlogSchema = new mongoose.schema(
-    {
-        title:{
-            type: String,
-            required:true
-        },
-        image:{
-            type: String,
-            required: true
-        },
-        slug:{
-            type: String,
-            required: true
-        },
-        createdby:{
-            
-        },
-        createddate:{
-            type: Date,
-            required:true
-        },
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    createdby: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team', // Assuming 'Team' is the collection for team members
+        required: true
+    },
+    createddate: {
+        type: Date,
+        required: true
     }
-)
+});
 
-const BlogModel = mongoose.model('Blogs', BlogSchema)
+const BlogModel = mongoose.model('Blogs', BlogSchema);
 
 module.exports = BlogModel;
