ce0887b527cdc5d544b2ea42942a912a7fb59ecd
jenith-acharya <kan077bct041@kec.edu.np>
2025-01-28 22:19:58 +0545

    some changes

--- LLS front/src/components/common/header/Navbarheader.tsx
+++ LLS front/src/components/common/header/Navbarheader.tsx
@@ -10,18 +10,18 @@ export const Homeheader = () => {
         <img src={Logo} className="size-40 sm:h-28 sm:w-64" alt="LLS Logo" />
       </Navbar.Brand>
       <div className="flex md:order-2">
-      <Button href="/contact" color="failure" style={{width:150 , height:45 , backgroundColor:"dark red", }} pill> CONTACT US </Button>
+      <Button href="/signin" color="failure" style={{width:150 , height:45 , backgroundColor:"dark red", }} pill> Sign In </Button>
         <Navbar.Toggle />
       </div>
       
       
       <Navbar.Collapse className="decoration-white ">
-        <Navbar.Link className="text-[15px] text-white " href="/" active>HOME</Navbar.Link >
-        <Navbar.Link className="text-[15px] text-white " href="/about">ABOUT US</Navbar.Link>
-        <Navbar.Link className="text-[15px] text-white "href="/ourteam">OUR TEAM</Navbar.Link>
-        <Navbar.Link className="text-[15px] text-white "href="/areaofpractice">AREAS OF PRACTICE</Navbar.Link>
-        <Navbar.Link className="text-[15px] text-white " href="/blogs">BLOGS</Navbar.Link>
-        <Navbar.Link className="text-[15px] text-white " href="/contact">CONTACT</Navbar.Link>
+        <Navbar.Link className="text-[15px] text-white " href="/" active> HOME</Navbar.Link >
+        <Navbar.Link className="text-[15px] text-white " href="/about"> ABOUT US</Navbar.Link>
+        <Navbar.Link className="text-[15px] text-white " href="/ourteam"> OUR TEAM</Navbar.Link>
+        <Navbar.Link className="text-[15px] text-white " href="/areaofpractice"> AREAS OF PRACTICE</Navbar.Link>
+        <Navbar.Link className="text-[15px] text-white " href="/blogs"> BLOGS</Navbar.Link>
+        <Navbar.Link className="text-[15px] text-white " href="/contact"> CONTACT</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
     </>
--- LLS front/src/config/router.config.tsx
+++ LLS front/src/config/router.config.tsx
@@ -3,6 +3,7 @@ import Landingpage from "../pages/landing";
 import Ourteam from "../pages/Our Team";
 import Areasofpractice from "../pages/Areasofpractice";
 import Aboutuspage from "../pages/About Us";
+import Signin from "../pages/signin/signin";
 
 
 
@@ -15,7 +16,8 @@ const Routerconfig =() =>{
             <Route path="/" element={<Landingpage/>}/>
             <Route path="/ourteam" element={<Ourteam/>}/>
             <Route path="/aboutus" element={<Aboutuspage/>}/> 
-            <Route path="/areaofpractice" element={<Areasofpractice/>}/>   
+            <Route path="/areaofpractice" element={<Areasofpractice/>}/> 
+            <Route path="/signin" element={<Signin/>} /> 
         </Routes>
         </BrowserRouter>
         </>
