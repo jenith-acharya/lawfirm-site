92be96cc2eb256ab2d716b12f3ca5b37329397d5
jenith-acharya <kan077bct041@kec.edu.np>
2025-01-29 19:43:41 +0545

    completion of service.js file

--- LLS back/src/modules/practiceareas/practice.service.js
+++ LLS back/src/modules/practiceareas/practice.service.js
@@ -1,23 +1,23 @@
-const BlogModel = require("./blogs.model");
+const PracticeModel = require("./practiceareas.model");
 
-class BlogService {
+class PracticeService {
     
-    // Create a new blog post
-    createBlog = async (data) => {
+    // Create a new practice area
+    createPractice = async (data) => {
         try {
-            const blog = new BlogModel(data);
-            return await blog.save();
+            const practice = new PracticeModel(data);
+            return await practice.save();
         } catch (exception) {
             throw exception;
         }
     };
     
-    // List all blog posts with pagination and filtering
+    // List all practice areas with pagination and filtering
     listData = async (currentPage, limit, filter = {}) => {
         try {
-            const count = await BlogModel.countDocuments(filter);
+            const count = await PracticeModel.countDocuments(filter);
             const skip = (currentPage - 1) * limit;
-            const data = await BlogModel.find(filter)
+            const data = await PracticeModel.find(filter)
                 .populate("createdby", ["_id", "name", "email"])
                 .skip(skip)
                 .limit(limit)
@@ -29,25 +29,25 @@ class BlogService {
         }
     };
     
-    // Get details of a blog post by filter
+    // Get details of a practice area by filter
     getDetailByFilter = async (filter) => {
         try {
-            const blogDetail = await BlogModel.findOne(filter).populate("createdby", ["_id", "name", "email"]);
-            if (!blogDetail) {
-                throw { statusCode: 404, message: "Blog post not found" };
+            const practiceDetail = await PracticeModel.findOne(filter).populate("createdby", ["_id", "name", "email"]);
+            if (!practiceDetail) {
+                throw { statusCode: 404, message: "Practice area not found" };
             }
-            return blogDetail;
+            return practiceDetail;
         } catch (exception) {
             throw exception;
         }
     };
     
-    // Delete a blog post by ID
+    // Delete a practice area by ID
     deleteById = async (id) => {
         try {
-            const response = await BlogModel.findByIdAndDelete(id);
+            const response = await PracticeModel.findByIdAndDelete(id);
             if (!response) {
-                throw { statusCode: 404, message: "Blog post not found" };
+                throw { statusCode: 404, message: "Practice area not found" };
             }
             return response;
         } catch (exception) {
@@ -55,12 +55,12 @@ class BlogService {
         }
     };
     
-    // Update a blog post by ID
+    // Update a practice area by ID
     updateById = async (id, data) => {
         try {
-            const response = await BlogModel.findByIdAndUpdate(id, data, { new: true });
+            const response = await PracticeModel.findByIdAndUpdate(id, data, { new: true });
             if (!response) {
-                throw { statusCode: 404, message: "Blog post not found" };
+                throw { statusCode: 404, message: "Practice area not found" };
             }
             return response;
         } catch (exception) {
@@ -68,28 +68,28 @@ class BlogService {
         }
     };
     
-    // Get details of a blog post by ID
+    // Get details of a practice area by ID
     getDetailById = async ({ _id }) => {
         try {
-            const blogDetail = await BlogModel.findById(_id).populate("createdby", ["_id", "name", "email"]);
-            if (!blogDetail) {
-                throw { statusCode: 404, message: "Blog post not found" };
+            const practiceDetail = await PracticeModel.findById(_id).populate("createdby", ["_id", "name", "email"]);
+            if (!practiceDetail) {
+                throw { statusCode: 404, message: "Practice area not found" };
             }
-            return blogDetail;
+            return practiceDetail;
         } catch (exception) {
             throw exception;
         }
     };
     
-    // List all published blogs
-    listAllBlogs = async () => {
+    // List all active practice areas
+    listAllPractices = async () => {
         try {
-            return await BlogModel.find({}, { title: 1 });
+            return await PracticeModel.find({}, { title: 1 });
         } catch (exception) {
             throw exception;
         }
     };
 }
 
-const blogService = new BlogService();
-module.exports = blogService;
+const practiceService = new PracticeService();
+module.exports = practiceService;
